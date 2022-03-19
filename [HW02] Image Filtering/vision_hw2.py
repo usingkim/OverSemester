04b4788f4c3d4c8f468b4414d7f4f105a35d2c20
YooJ-K <kimyujin173@gmail.com>
2022-03-20 03:14:18 +0900

    almost done HW2 Part 1

--- [HW02] Image Filtering/vision_hw2.py
+++ [HW02] Image Filtering/vision_hw2.py
@@ -47,4 +47,43 @@ def gauss2d(sigma):
 # 4.
 # 4-a. zero paddings
 def convolve2d(array, filter):
-    
+    # f x f kernel, m = (f-1)/2 : m space < fill with zeros
+    f = len(filter)
+    m = (f-1) // 2
+    # add zero padding
+    array_padding = np.pad(array, ((m, m), (m, m)), 'constant', constant_values=0)
+    return array_padding
+
+# array = np.array(
+#     ([1, 0, 0, 0, 1],
+#     [2, 3, 0, 8, 0],
+#     [2, 0, 0, 0, 3],
+#     [0, 0, 1, 0, 0])
+# )
+# filter = np.array(
+#     ([0, 0, 0],
+#     [0, 0, 0],
+#     [0, 0, 0])
+# )
+
+# print(convolve2d(array, filter))
+
+# 4-b. Gaussian convolution to a 2D array
+def gaussconvolve2d(array, sigma):
+    # generating a filter with gauss2d
+    filter = gauss2d(sigma)
+    # apply it to the array with convolve2d
+    padding = convolve2d(array, filter)
+    return padding
+
+# 4-c. Apply gaussconvolve2d
+# Load Dog Image into Python
+dog = Image.open('[HW02] Image Filtering/hw2_image/2b_dog.bmp')
+# Convert it to a greyscale
+grey_dog = dog.convert('L')
+grey_dog = gaussconvolve2d(grey_dog, 3)
+grey_dog = Image.fromarray(grey_dog)
+
+# 4-d. Use PIL to show both the original and filtered images.
+dog.show()
+grey_dog.show()
