ef29b72d0919dd0ecf3fe64fe1aa6cda8d36c1f9
YooJ-K <kimyujin173@gmail.com>
2022-05-02 15:39:53 +0900

    Example05 finish

--- Week07/Example05/app/src/main/java/com/example/myapplication/Tab2Fragment.kt
+++ Week07/Example05/app/src/main/java/com/example/myapplication/Tab2Fragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.Tab2FragmentBinding
 
 class Tab2Fragment : Fragment() {
@@ -17,5 +19,18 @@ class Tab2Fragment : Fragment() {
     ): View? {
         binding = Tab2FragmentBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        
+        val datas = mutableListOf<String>()
+        for(i in 1..20) {datas.add("Item $i")}
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
+        binding.recyclerView.adapter = MyAdapter(datas)
+        binding.recyclerView.addItemDecoration(
+            DividerItemDecoration(this.context, LinearLayoutManager.VERTICAL)
+        )
     }
 }
