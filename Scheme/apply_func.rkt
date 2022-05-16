af068fc7e947e2f8b3fba66c6f10dddcbae77f76
YooJ-K <kimyujin173@gmail.com>
2022-05-16 17:21:03 +0900

    class at 05/12

---
+++ Scheme/apply_func.rkt
@@ -0,0 +1,14 @@
+(lambda (x y) (+ x y))
+
+(define (add x y) (+ x y))
+(define add 5)
+add
+
+(define add (lambda (x y) (+ x y)))
+(add 1 1)
+
+(define vhello "Hello World")
+vhello
+(define fhello (lambda () "Hello World"))
+(fhello)
+;diffrent each other
---
+++ Scheme/double.rkt
@@ -0,0 +1,1 @@
+(define (double n) (+ n n))
